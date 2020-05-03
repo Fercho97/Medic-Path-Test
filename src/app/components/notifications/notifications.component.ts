247f5a5c1771d800af933286a407bdb21937cce0
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-05-02 19:50:41 -0500

    Error message notifications

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -4,6 +4,7 @@ import {NotificacionService} from './notifications.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { InfoMedicalRecordComponent } from '../profile/medical-record/info-medical-record/info-medical-record.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -14,7 +15,8 @@ export class NotificationsComponent implements OnInit {
   public notificaciones = [];
   content;
   constructor(private notifServ : NotificacionService, private storage: CryptoStorage,
-              private modalService : NgbModal, private spinner : NgxSpinnerService) {
+              private modalService : NgbModal, private spinner : NgxSpinnerService,
+              private toast: ToastrService) {
    }
 
   ngOnInit() {
@@ -41,6 +43,7 @@ export class NotificationsComponent implements OnInit {
     },
   error =>{
     this.spinner.hide();
+    this.toast.error('Hubo un error al conseguir sus notificaciones, favor de intentarlo de nuevo', 'Error')
       //console.log(error);
   })
   }
