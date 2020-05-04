d8582034a61d048eb10e095f2e90df07b5d365d4
Cristopher Estrada <crissin21_01@hotmail.com>
2020-05-04 12:47:24 -0500

    images fixed

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  selector: "app-faq",
+  templateUrl: "./faq.component.html",
+  styleUrls: ["./faq.component.css"],
 })
 export class FaqComponent implements OnInit {
+  images = [
+    {
+      img: "assets/faq.jpg",
+    },
+  ];
 
   active = false;
 
@@ -24,14 +29,17 @@ export class FaqComponent implements OnInit {
     this.preguntas[2] = "¿Como funciona la página?";
     this.preguntas[3] = "¿Se necesita algún tipo de subscripción?";
 
-    this.respuestas[0] = "Un grupo que se encuentra dedicado al cuidado de la salud con el apoyo de los mejores especialistas en los campos de medicina";
-    this.respuestas[1] = "Brindar información y apoyo para consultas médicas de nuestros usuarios";
-    this.respuestas[2] = "Tan simple como registrarse y comenzar a disfrutar los beneficios y servicios de la página";
-    this.respuestas[3] = "Por supuesto que no, la página y los servicios son siempre gratuitos de uso siempre y cuando usted tenga una cuenta en la página";
-   }
-
-  ngOnInit() {
+    this.respuestas[0] =
+      "Un grupo que se encuentra dedicado al cuidado de la salud con el apoyo de los mejores especialistas en los campos de medicina";
+    this.respuestas[1] =
+      "Brindar información y apoyo para consultas médicas de nuestros usuarios";
+    this.respuestas[2] =
+      "Tan simple como registrarse y comenzar a disfrutar los beneficios y servicios de la página";
+    this.respuestas[3] =
+      "Por supuesto que no, la página y los servicios son siempre gratuitos de uso siempre y cuando usted tenga una cuenta en la página";
   }
+
+  ngOnInit() {}
 
   showAnswer(id) {
     if (this.botones[id]) {
@@ -40,5 +48,4 @@ export class FaqComponent implements OnInit {
       this.botones[id] = true;
     }
   }
-
 }
