d585150b9a0b186d485a0e3908a7a01ca8751d1e
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-05-06 12:00:07 -0500

    Correciones ortográficas

--- src/app/components/home-info/home-info.component.ts
+++ src/app/components/home-info/home-info.component.ts
@@ -12,26 +12,26 @@ export class HomeInfoComponent implements OnInit {
       li1:
         "Señala los síntomas utilizando la herramienta didáctica e intuitiva.",
       li2:
-        "Contamos con presicion del 99% gracias a nuestros algoritmos de Inteligencia Artificial.",
-      li3: "Apoyado y valorado por medicos con mas de 30 años de carrera.",
+        "Resultados que te apoyaran a saber que es lo que padeces",
+      li3: "Apoyado y valorado por médicos con mas de 30 años de carrera.",
       img: "assets/sintomas-main.jpg",
     },
     {
       header: "Queremos que te sientas seguro",
       li1:
-        "Nuestro objetivo es brindarte confianza en los diagnosticos y llevar a cabo un seguimiento de tu estado de salud.",
+        "Nuestro objetivo es brindarte confianza en los diagnósticos y llevar a cabo un seguimiento de tu estado de salud.",
       li2: "Puedes acceder desde cualquier dispositivo.",
       li3:
-        "Contamos con una aplicacion movil para brindarte mayor comodidad y accesibilidad.",
+        "Contamos con una aplicación móvil para brindarte mayor comodidad y accesibilidad.",
       img: "assets/responsive-main.png",
     },
     {
       header: "Actualizamos nuestro sistema todo el tiempo",
-      li1: "La pagina se actualiza para brindarte una mejor experiencia.",
+      li1: "La página se actualiza para brindarte una mejor experiencia.",
       li2:
-        "Con ayuda de expertos, ampliamos nuestros diagnosticos a diversas areas.",
+        "Con ayuda de expertos, ampliamos nuestros diagnósticos a diversas áreas.",
       li3:
-        "Los medicos y especialistas brindan su conocimiento para hacer de este sistema mas fiable.",
+        "Los médicos y especialistas brindan su conocimiento para hacer de este sistema mas fiable.",
       img: "assets/update-main.jpg",
     },
   ];
