47dac118afeaffc5a6b89ea617c5f1c73f314b52
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-05-09 15:46:44 -0500

    Changes on user edit

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -20,7 +20,7 @@ export class FaqComponent implements OnInit {
 
   respuestas = [];
   constructor() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
       this.botones[i] = this.active;
     }
 
@@ -28,6 +28,7 @@ export class FaqComponent implements OnInit {
     this.preguntas[1] = "¿A qué nos dedicamos?";
     this.preguntas[2] = "¿Como funciona la página?";
     this.preguntas[3] = "¿Se necesita algún tipo de subscripción?";
+    this.preguntas[4] = "¿Me equivoque al registrarme y puse algún dato personal erróneo, que hago?"
 
     this.respuestas[0] =
       "Un grupo que se encuentra dedicado al cuidado de la salud con el apoyo de los mejores especialistas en los campos de medicina";
@@ -37,6 +38,8 @@ export class FaqComponent implements OnInit {
       "Tan simple como registrarse y comenzar a disfrutar los beneficios y servicios de la página";
     this.respuestas[3] =
       "Por supuesto que no, la página y los servicios son totalmente gratuitos, no se necesita de suscripción ni nada por el estilo";
+    this.respuestas[4] =
+      "En ese caso le recomendamos ponerse en contacto en el siguiente correo: medicpath.services@gmail.com, allí se analizará su caso y le daremos una solución";
   }
 
   ngOnInit() {}
--- src/app/components/profile/profile-info/profile-info.component.ts
+++ src/app/components/profile/profile-info/profile-info.component.ts
@@ -24,18 +24,6 @@ export class ProfileInfoComponent implements OnInit {
   constructor(private profileServ : ProfileService, private toast : ToastrService, 
               private router : Router, private nickVal : NicknameValidator) {
     this.datos_perfil = new FormGroup({
-      nombres : new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-        Validators.pattern('^([ñÑáÁéÉíÍóÓúÚüÜa-zA-Z]+ )*[ñÑáÁéÉíÍóÓúÚüÜa-zA-Z]+$')
-      ]),
-      apellidos : new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-        Validators.pattern('^([ñÑáÁéÉíÍóÓúÚüÜa-zA-Z]+ )*[ñÑáÁéÉíÍóÓúÚüÜa-zA-Z]+$')
-      ]),
       nickname : new FormControl('', [
         Validators.required,
         Validators.minLength(3),
@@ -51,12 +39,14 @@ export class ProfileInfoComponent implements OnInit {
   actualizarDatos(){
     //console.log(this.datos_perfil.value);
     this.formData.append('nickname', this.datos_perfil.value.nickname);
-    this.formData.append('nombres', this.datos_perfil.value.nombres);
-    this.formData.append('apellidos', this.datos_perfil.value.apellidos);
         this.profileServ.updateUser(this.usuarioInfo.hash_id, this.formData, sessionStorage.getItem('token')).subscribe( (res: any) =>{
           //console.log(res);
           if(res.body.mensaje != "Token no válido"){
-          this.toast.success('Datos Modificados con éxito', 'Modificación Exitosa!');
+            if(res.body.message=="No hubo cambios"){
+              this.toast.info(res.body.message);
+            }else{
+                this.toast.success("Se actualizo su nombre de usuario con éxito", 'Modificación Exitosa!');
+            }
           this.formData = new FormData();
           sessionStorage.setItem('token', res.body.token);
           }else{
