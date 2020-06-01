78778105fad0e0ce6a59e3f306ccb37f9c38ea9d
unknown <fernando.jaime@alumnos.udg.mx>
2020-06-01 10:26:22 -0500

    Fix date validator

--- src/app/validators/DateValidator.ts
+++ src/app/validators/DateValidator.ts
@@ -17,7 +17,7 @@ export class DateValidator {
         let controlDate = moment(control.value).format('L');
         let years = today.diff(control.value, 'years');
         //console.log(years);
-        if(years > 16){
+        if((years > 16 && years < 99) || controlDate > todayDate){
             return null
         }else{
             return{'noValidAge': true}  
